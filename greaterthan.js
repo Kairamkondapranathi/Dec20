59559c1fc06141a6bed89da743849f78479422df
Pranathi <89980698+Kairamkondapranathi@users.noreply.github.com>
2022-01-01 22:24:44 +0530

    Add files via upload

---
+++ greaterthan.js
@@ -0,0 +1,7 @@
+var num=5;
+if(num>10){
+console.log("Number is Greater than 10");
+}
+else{
+console.log("Number is not Greater than 10");
+}
---
+++ greatest.js
@@ -0,0 +1,13 @@
+var a=10;
+var b=20;
+var c=15;
+
+if(a>b){
+console.log(a);
+}
+else if(b>c){
+console.log(b);
+}
+else{
+console.log(c);
+}
